9c541fdb5e37080140f322210eb8bbee5ff7bd7b
infauraga <infauraga@PINFMSI01.elorrieta.int>
2022-03-17 11:51:51 +0100

    ejemplo exception

--- src/main/java/com/elorrieta/basico/LeerEntero.java
+++ src/main/java/com/elorrieta/basico/LeerEntero.java
@@ -4,26 +4,34 @@ import java.util.Scanner;
 
 /**
  * Ejemplo basico para leer un numero entero (int) por teclado
+ * 
  * @author infauraga
  *
  */
 public class LeerEntero {
 
 	public static void main(String[] args) {
-System.out.println("dime tu nombre:");
-		
+		System.out.println("dime un numero:");
+
 		Scanner sc = new Scanner(System.in);
-		
+
 		// decalramos una variable de tipo entero
 		int numero = 0;
-		
+
 		// leemos por pantalla y lo guaradmos en la variable
-		
-		
-		System.out.println("El numero es " + numero);		
-		
-		System.out.println("Termina programa");
-		sc.close();
+		try {
+			numero = Integer.parseInt(sc.nextLine());
+			System.out.println("El numero es " + numero);
+			
+		}catch (Exception e) {
+
+			System.err.println("Hemos tenido una excepcion " + e.getMessage());
+			e.printStackTrace(); // muestra la traza de la Exception
+			
+		}finally {
+			System.out.println("Termina programa");
+			sc.close();
+		}
 
 	}
 
