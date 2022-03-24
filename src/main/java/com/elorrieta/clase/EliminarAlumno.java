7a9a00c2ae4b23b7a7f3c2ef21a6a65a775c3df1
infauraga <infauraga@PINFMSI01.elorrieta.int>
2022-03-24 12:43:26 +0100

    modificar y eliminar alumno

---
+++ src/main/java/com/elorrieta/clase/EliminarAlumno.java
@@ -0,0 +1,40 @@
+package com.elorrieta.clase;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+public class EliminarAlumno {
+
+	public static void main(String[] args) {
+
+				
+		try {
+		
+			//TODO pedir datos por pantalla
+			int idAlumno = 2;
+		
+			
+			Connection conexion = DriverManager.getConnection("jdbc:mysql://localhost:3306/clase", "root", "root");
+			String sql = "DELETE FROM alumno WHERE id_alumno = ?;";
+			PreparedStatement pst = conexion.prepareStatement(sql);
+					
+			pst.setInt(1, idAlumno);			
+			
+			pst.executeUpdate(); 
+			
+			System.out.println("Alumno Eliminado");
+	
+		}catch (Exception e) {
+			
+			System.out.println("Hemos tenido un problema");
+			e.printStackTrace();
+		}	
+		
+
+		System.out.println("Termina el programa");
+		
+	}// main
+
+}// clase
---
+++ src/main/java/com/elorrieta/clase/ModificarAlumno.java
@@ -0,0 +1,45 @@
+package com.elorrieta.clase;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+public class ModificarAlumno {
+
+	public static void main(String[] args) {
+
+				
+		try {
+		
+			//TODO pedir datos por pantalla
+			int idAlumno = 2;
+			String email = "ibai2222@gmail.com";
+			String pass = "1234567890";
+			
+			
+			Connection conexion = DriverManager.getConnection("jdbc:mysql://localhost:3306/clase", "root", "root");
+			String sql = "UPDATE alumno SET email = ? , pass= ? WHERE id_alumno = ?;";
+			PreparedStatement pst = conexion.prepareStatement(sql);
+			
+			pst.setString(1, email);
+			pst.setString(2, pass);
+			pst.setInt(3, idAlumno);
+			
+			
+			pst.executeUpdate(); 
+			
+			System.out.println("Alumno modificado");
+	
+		}catch (Exception e) {
+			
+			System.out.println("Hemos tenido un problema");
+			e.printStackTrace();
+		}	
+		
+
+		System.out.println("Termina el programa");
+		
+	}// main
+
+}// clase
